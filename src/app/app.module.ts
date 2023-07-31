7e2d8b9bd6fd74637d407cf7271f93be02ccea39
Richard Leurs <richard.leurs@elastique.nl>
2023-07-31 11:08:46 +0200

    Add dropdown header menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -135,6 +136,7 @@ import { HomeTilesComponent } from './components/home-tiles/home-tiles.component
     NoopAnimationsModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatMenuModule,
     MatSidenavModule,
     MatButtonModule,
     MatIconModule,
