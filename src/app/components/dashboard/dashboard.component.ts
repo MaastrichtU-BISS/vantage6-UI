c95a607d6e92e22ae687e66bdede02b39d37de65
Carlos <carloseam94@gmail.com>
2023-10-17 11:06:03 +0200

    add: load data from json

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+type DashBoardData = {
+  type: string;
+  value: any;
+};
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  data: DashBoardData[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  loadData(): void {
+    fetch('../../../assets/dashboard_data.json').then((file) => {
+      file.json().then((json) => {
+        this.data = json;
+        console.log(this.data);
+      });
+    });
   }
 
+  ngOnInit(): void {
+    this.loadData();
+  }
 }
